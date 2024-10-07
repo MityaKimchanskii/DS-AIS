9e5007b184d3369fb217c839aab09a13ba7703ef
Dmitrii Kim <mitya_kim@icloud.com>
2024-10-06 17:50:32 -0700

    challenge 4 and 5

--- Linked_List.playground/Pages/Challenge4.xcplaygroundpage/Contents.swift
+++ Linked_List.playground/Pages/Challenge4.xcplaygroundpage/Contents.swift
@@ -9,10 +9,73 @@
 // merged list
 // -1 -> 1 -> 2 -> 3 -> 4 -> 6 -> 10 -> 11
 
-func mergeSorted<T>(_ linkedList: LinkedList<T>?) {
-    guard !left.isEmpty else { return }
-    guard !right.isEmpty else { return }
+func mergeSorted<T: Comparable>(_ left: LinkedList<T>, _ right: LinkedList<T>) -> LinkedList<T> {
+    guard !left.isEmpty else { return right }
+    guard !right.isEmpty else { return left }
     
     var newHead: Node<T>?
     var tail: Node<T>?
+    
+    var currentLeft = left.head
+    var currentRight = right.head
+    
+    if let leftNode = currentLeft, let rightNode = currentRight {
+        if leftNode.value < rightNode.value {
+            newHead = leftNode
+            currentLeft = leftNode.next
+        } else {
+            newHead = rightNode
+            currentRight = rightNode.next
+        }
+        tail = newHead
+    }
+    
+    while let leftNode = currentLeft, let rightNode = currentRight {
+        if leftNode.value < rightNode.value {
+            tail?.next = leftNode
+            currentLeft = leftNode.next
+        } else {
+            tail?.next = rightNode
+            currentRight = rightNode.next
+        }
+        tail = tail?.next
+    }
+    
+    if let leftNodes = currentLeft {
+        tail?.next = leftNodes
+    }
+    
+    if let rightNodes = currentRight {
+        tail?.next = rightNodes
+    }
+    
+    var list = LinkedList<T>()
+    list.head = newHead
+    list.tail = {
+        while let next = tail?.next {
+            tail = next
+        }
+        return tail
+    }()
+    
+    return list
+}
+
+example(of: "merging two lists") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    var list2 = LinkedList<Int>()
+    list2.append(5)
+    list2.append(6)
+    list2.append(7)
+    
+    print("First list: \(list)")
+    print("Second list: \(list2)")
+    
+    let mergedList = mergeSorted(list, list2)
+    print("MergedList: \(mergedList)")
+    
 }
--- Linked_List.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
+++ Linked_List.playground/Pages/Challenge5.xcplaygroundpage/Contents.swift
@@ -5,3 +5,47 @@
 // 1 -> 3 -> 3 -> 3 -> 4
 // list after removing all occurrences of 3
 // 1 -> 4
+
+extension LinkedList where Value: Equatable {
+    
+    mutating func removeAll(_ value: Value) {
+        
+        while let head = self.head, head.value == value {
+            self.head = head.next
+        }
+        
+        var prev = head
+        var current = head?.next
+        
+        while let currentNode = current {
+            if currentNode.next == nil {
+                tail
+            }
+            
+            guard currentNode.value != value else {
+                prev?.next = currentNode.next
+                current = prev?.next
+                continue
+            }
+            
+            prev = current
+            current = current?.next
+        }
+        tail = prev
+    }
+}
+
+example(of: "deleting all occurences of a specific element") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(2)
+    list.push(2)
+    list.push(1)
+    list.push(1)
+    
+    print("Before deleteng 2 from the linked list: \(list)")
+    
+    list.removeAll(2)
+    print("After deleting 2 from the linked list \(list)")
+}
