bc9661bcba15d686cd9e351875102f0066557574
Dmitrii Kim <mitya_kim@icloud.com>
2024-10-21 16:34:20 -0700

    add challenge binary tree

--- BinaryTrees.playground/Contents.swift
+++ BinaryTrees.playground/Contents.swift
@@ -27,10 +27,64 @@ example(of: "in order traversal") {
     tree.traverseInOrder { print($0) }
 }
 
-example(of: "pre order traversal") {
-    tree.traversePreOrder { print($0) }
-}
+//example(of: "pre order traversal") {
+//    tree.traversePreOrder { print($0) }
+//}
 
 example(of: "post order traversal") {
     tree.traversePostOrder { print($0) }
 }
+
+// Challenge 1 - find the height of the tree
+func height<T>(of node: BinaryNode<T>?) -> Int {
+    guard let node else { return -1 }
+    return 1 + max(height(of: node.leftChild), height(of: node.rightChild))
+}
+
+height(of: tree)
+
+// Challenge 2 - serialize or deserialize tree
+extension BinaryNode {
+    public func traversePreOrder(visit: (Element?) -> ()) {
+        visit(value)
+        
+        if let leftChild = leftChild {
+            leftChild.traversePreOrder(visit: visit)
+        } else {
+            visit(nil)
+        }
+        
+        if let rightChild = rightChild {
+            rightChild.traversePreOrder(visit: visit)
+        } else {
+            visit(nil)
+        }
+    }
+}
+
+func serialize<T>(_ node: BinaryNode<T>) -> [T?] {
+    var array: [T?] = []
+    node.traversePreOrder { array.append($0) }
+    return array
+}
+
+func deserialize<T>(_ array: [T?]) -> BinaryNode<T>? {
+    var reversed = Array(array.reversed())
+    return deserialize(&reversed)
+}
+
+func deserialize<T>(_ array: inout [T?]) -> BinaryNode<T>? {
+    guard !array.isEmpty, let value = array.removeLast() else { return nil }
+    let node = BinaryNode(value: value)
+    node.leftChild = deserialize(&array)
+    node.rightChild = deserialize(&array)
+    return node
+}
+
+var array = serialize(tree)
+print(array)
+let node = deserialize(&array)
+let node2 = deserialize(array)
+print(node!)
+
+
